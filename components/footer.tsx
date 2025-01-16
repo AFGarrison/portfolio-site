ab6bb072dac7c5abd3aa3897bf38faf85d896e9c
AFGarrison <75108940+AFGarrison@users.noreply.github.com>
2025-01-16 15:24:29 +0900

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className="one-more-thing center-text">
-            <p><em>Site built by Andrew Garrison, using Fresh.js and published on Deno Deploy</em></p>
+            <p><em>Site built by Andrew Garrison using Fresh.js and published on Deno Deploy</em></p>
         </div>
     )
 }
